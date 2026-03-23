0c940eee9ddb269c5a3da969f38790743323ffe3
Joseph D. Carpinelli <joseph.d.carpinelli@loopy.codes>
2026-03-22 22:55:18 -0400

    chore: add `gingembre`

--- crates/git-template/src/lib.rs
+++ crates/git-template/src/lib.rs
@@ -1,1 +1,14 @@
 //! Template repository support for Git.
+
+use git_vendor::Vendor as _;
+
+use git2::Error;
+
+type Result<T = ()> = std::result::Result<T, Error>;
+
+/// Temp.
+pub trait TemplateRepository {
+    fn from_template() -> Result {
+        Ok(())
+    }
+}
